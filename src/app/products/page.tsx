935ec0a3bcfe6eef10dfa669ff11959b4e0f870e
asafebxd <alves.asafecabral@gmail.com>
2025-02-19 19:40:44 -0300

    feat: add shadcn initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const ProductsPage = () => {
+  return (
+    <div className="pt-5 border border-red-500 rounded-xl">
+      <h1 className="text-red-500">products</h1>;<Button>FSW 7.0</Button>
+      <Input placeholder="Bora fechar esse projeto" />
+    </div>
+  );
+};
+
+export default ProductsPage;
